d39653ad54256a1731cc0a26f47324d49058480e
roiat <roia1@mail.sapir.ac.il>
2026-01-15 12:22:58 +0200

    added rules and neigborhood logic

--- src/Conways_game_of_Life/Neighborhood_Logic.java
+++ src/Conways_game_of_Life/Neighborhood_Logic.java
@@ -1,5 +1,20 @@
 package Conways_game_of_Life;
 
-public class Neighborhood_Logic
-{
+public class Neighborhood_Logic {
+    public int getNextState(int currentState, int aliveNeighbors) {
+        if(currentState == 1) { // Cell is currently alive
+            if(aliveNeighbors < 2 || aliveNeighbors > 3) {
+                return 0; // Cell dies
+            } else {
+                return 1; // Cell lives
+            }
+        } else { // Cell is currently dead
+            if(aliveNeighbors == 3) {
+                return 1; // Cell becomes alive
+            } else {
+                return 0; // Cell remains dead
+            }
+        }
+    }
+
 }
--- src/Conways_game_of_Life/Rules.java
+++ src/Conways_game_of_Life/Rules.java
@@ -1,4 +1,16 @@
 package Conways_game_of_Life;
 
 public class Rules {
+    public int countAliveNeighbors(Board board , int r, int c) {
+        int count = 0;
+        for(int i = r -1; i < r + 1 ; i++ ) {
+            for(int j = c - 1; j < c + 1; j++) {
+                if(i == r && j == c) {
+                   count += board.getCells(i, j);
+                }
+            }
+        }
+        return count;
+    }
+
 }
